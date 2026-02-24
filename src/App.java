0abf85499007f7c2a6112d9e947e3b296a17bc2c
danu44-1 <danudimas042@gmail.com>
2026-02-24 16:10:01 +0700

    Latihan Modul 1_commit ketiga

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Danu Dimas Putra");
         System.out.println("105224003");
+        System.out.println("_");
     }
 }
